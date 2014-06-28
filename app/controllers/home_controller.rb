732ec95f4cfb0f66efac350214615ffe46d4834f
Kevin Adler <adler_187@yahoo.com>
2014-06-27 23:51:16 -0500

    Add qso and point totals for score table

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,15 @@
 class HomeController < ApplicationController
   def index
-    @lastid = params[:lastid] || 0
+    @lastid = params[:lastid] || (Qso.last.id - 100)
     
-    @qsos = Qso.where('id > ?', @lastid).limit(10)
+    @qsos = Qso.where('id > ?', @lastid).limit(100)
     
     @lastid = @qsos.last.id unless @qsos.empty?
     
     @rigs = Rig.all
+    
+    @total_qsos = 0
+    @total_points = 0
     
     @scores = []
     Qso.valid_bands.each do |band|
@@ -18,14 +21,20 @@ class HomeController < ApplicationController
         cw_points = cw_count*2
         dig_points = dig_count*2
         
+        band_qsos = ssb_count + cw_count + dig_count
+        band_points = ssb_points + cw_points + dig_points
+        
         @scores << {
           name: Qso.band_to_s(band),
           ssb: ssb_count,
           cw: cw_count,
           dig: dig_count,
-          qsos: ssb_count + cw_count + dig_count,
-          points: ssb_points + cw_points + dig_points
+          qsos: band_qsos,
+          points: band_points
         }
+        
+        @total_points += band_points
+        @total_qsos += band_qsos
     end
     
     respond_to do |format|
